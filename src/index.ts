b2cade962de41bbbb065edc6b5d540eda1ac2a44
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-06-07 17:01:59 -0300

    feat: index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from './morningGreet';
+export * from './eveningGreet';
+// This code exports functions from two different files: morningGreet and eveningGreet.
+// The morningGreet function generates a personalized morning greeting,
+// while the eveningGreet function generates a personalized evening greeting.
+// This allows users to import both functions from a single entry point,
+// making it easier to use them in their applications.
+// The morningGreet function can be used to greet someone in the morning,
+// while the eveningGreet function can be used to greet someone in the evening.
